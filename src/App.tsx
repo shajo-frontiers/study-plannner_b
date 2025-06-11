30ae79be0653c4c210b65a3c298f63721d64a574
AKT-nk <a23.wwmp@g.chuo-u.ac.jp>
2025-06-11 14:58:27 +0900

    6/11 UX論追加

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import Description from './Description'
 function App() {
   const events:TimetableEvent[] = [
     {day: "Wed", period: 2, title: "情報システム設計"},
-    {day: "Wed", period: 3, title: "ITビジネスのフロンティア"}
+    {day: "Wed", period: 3, title: "ITビジネスのフロンティア"},
+    {day: "Wed", period: 4, title: "UX論"}
   ];
 
   return (
--- src/Description.tsx
+++ src/Description.tsx
@@ -1,7 +1,9 @@
 function Description(){
-
+    const username = "中央太郎";
+    const today = (new Date()).getDate();
     return(
         <section> 
+             
         <p><strong>時間割を計画するアプリです</strong></p>
         <p>中央大学生only</p>
         </section>
