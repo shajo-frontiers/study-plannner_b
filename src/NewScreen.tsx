8c2ec08d71673662252f71078a61ccafddaab7ff
nayn001 <a23.kahs@g.chuo-u.ac.jp>
2025-07-02 14:28:49 +0900

    画面遷移とBootstrap導入

---
+++ src/NewScreen.tsx
@@ -0,0 +1,14 @@
+// src/NewScreen.tsx
+import React from 'react';
+
+function NewScreen() {
+    return (
+        <div>
+            <h1>過去の時間割</h1>
+            <button type="button" class="btn btn-info">時間割①</button>
+            <p></p>
+        </div>
+    );
+}
+
+export default NewScreen;
