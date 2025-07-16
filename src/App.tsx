be6c77b6489af42e67f06ce2267e8d7481a4778e
AKT-nk <a23.wwmp@g.chuo-u.ac.jp>
2025-07-16 14:09:53 +0900

    7/16 統合

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import './App.css';
 import TimeTable, {TimetableEvent} from './TimeTable'
 import Description from './Description'
 import SearchClassName from './SearchClassName'
+import React, { useState } from 'react';
+import NewScreen from './NewScreen';
+import SettingsScreen from './SettingsScreen'; 
 import AddtimetableIcon from './AddtimetableIcon'
 
 
@@ -10,6 +13,12 @@ function App() {
     {day: "Wed", period: 2, title: "情報システム設計"},
     {day: "Wed", period: 3, title: "ITビジネスのフロンティア"}
   ];
+
+  const [screen, setScreen] = useState('main'); // 画面の状態を管理
+
+  const handleSetButtonClick = () => {
+    setScreen('new'); // 画面を'new'に切り替え
+  };
 
   const username1 = '中央太郎'
   return (
@@ -21,6 +30,19 @@ function App() {
       <div className="controls"><AddtimetableIcon /></div>
       <div className="controls2"><SearchClassName /></div>
       <div className="timetable"><TimeTable events={events} /></div>
+      {screen === 'main' ? (
+        <>
+          <Description />
+          <SearchClassName />
+          <TimeTable events={events}></TimeTable>
+          <div className="button-container">
+           <button className="add-event-button2" onClick={handleSetButtonClick}>セット</button> {/* onClickイベントハンドラを追加 */}
+           <button className="add-event-button1">設定</button>
+          </div>
+        </>
+      ) : (
+        <NewScreen />
+      )}
     </>
   );
 }
